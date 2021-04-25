9a85e366c38e7daba371741b0ea9f0e525f8fae6
websunsey <websunsey@gmail.com>
2021-04-25 21:44:40 +0300

    update

--- scripts/howto.js
+++ scripts/howto.js
@@ -2,32 +2,32 @@
   var pagescount, i, txt = "", activePage, fromPos, toPos, x, prevPage, nextPage, able = "";
   pagescount = 8;
   activePage = window.location.pathname;
-  if (activePage.indexOf(".html") == -1) {activePage = "index.html";}
-  if (activePage.indexOf("index.html") > -1) {
+  if (activePage.indexOf(".php") == -1) {activePage = "index.php";}
+  if (activePage.indexOf("index.php") > -1) {
     activePage = 1;
   } else {
     fromPos = activePage.indexOf("index_page") + 12;
-    toPos = activePage.indexOf(".html");
+    toPos = activePage.indexOf(".php");
     activePage = activePage.substring(fromPos, toPos);
   }
   if (activePage == 1) {
     able = " pagdisabled"
-    prevPage = "index.html"
+    prevPage = "index.php"
   } else if (activePage == 2) {
-    prevPage = "index.html";
+    prevPage = "index.php";
   } else {
-    prevPage = "index_page" + (activePage - 1) + ".html";
+    prevPage = "index_page" + (activePage - 1) + ".php";
   }
   if (activePage == pagescount) {
-    nextPage = "index.html";
+    nextPage = "index.php";
   } else {
-    nextPage = "index_page" + (parseInt(activePage) + 1) + ".html";
+    nextPage = "index_page" + (parseInt(activePage) + 1) + ".php";
   }
   for (i = 1; i <= pagescount; i++) {
-    defPage = "index_page" + i + ".html";
+    defPage = "index_page" + i + ".php";
     if (i == 1) {
       txt += "<a class='howtopag_item" + able + "' href='" + prevPage + "'>&#10094;</a>";
-      defPage = "index.html";
+      defPage = "index.php";
     }
     txt += "<a class='howtopag_item' href='" + defPage + "'>" + i + "</a>";
     if (i == pagescount) {
